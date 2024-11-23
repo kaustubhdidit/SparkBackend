61468894909ebdfcf88bfc1ecc2919cb4c27777c
kaustubhdidit <92023578+kaustubhdidit@users.noreply.github.com>
2024-11-23 20:22:20 +0530

    CORS

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',  // Allow requests from this origin
+    // origin: 'http://localhost:3000',  // Allow requests from this origin
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],  // Add PATCH to the allowed methods
     credentials: true,
   })
