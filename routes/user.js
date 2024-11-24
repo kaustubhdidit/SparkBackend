dd2774d8336952d2416e194412be8144a25f60f8
kaustubhdidit <92023578+kaustubhdidit@users.noreply.github.com>
2024-11-24 16:04:29 +0530

    Delete functionality

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ import express from "express";
 import {login} from "../controllers/login.js";
 import {register} from "../controllers/register.js";
 import { getAllUsers } from "../controllers/user.js";
-import { updatePermissions } from "../controllers/user.js";
+import { updatePermissions, deleteUser } from "../controllers/user.js";
 
 const router = express.Router();
 console.log("first")
@@ -10,6 +10,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/getAllUsers", getAllUsers);
 router.patch("/updatePermissions/:id", updatePermissions); // :id captures the user ID
+router.delete("/delete/:id", deleteUser);
 
 
 export default router;
